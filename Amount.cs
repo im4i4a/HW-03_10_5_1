5cd7af2fdc203a47f63fedb77ae8f581cf9343f5
im4i4a <al.zemskov03@yandex.ru>
2025-11-08 20:57:45 +0300

    Сделал первое задание домашней работы

---
+++ Amount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_03_10_5_1
+{
+    internal class Amount
+    {
+    }
+
+    public interface IAmount 
+    {
+        void Sum(int a, int b);
+
+    }
+    public class Summary : IAmount
+    {
+        public int A {  get; set; }
+        public int B { get; set; }
+        public int Result {  get; set; }
+
+        public Summary(int a, int b) 
+        {
+            A = a; 
+            B = b;
+        }
+
+        void IAmount.Sum(int a, int b)
+        {
+            int result = a + b;
+            Result = result;      
+        }
+        public void Output()
+        {
+            Console.WriteLine($"Результат сложения {A} и {B} равно {Result}");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Задание 1
+            int a,b;
+
+            while (true)
+            {
+                try
+                {
+                    a = Convert.ToInt32(Console.ReadLine());
+                    b = Convert.ToInt32(Console.ReadLine());
+
+                    Summary summary = new Summary(a, b);
+
+                    ((IAmount)summary).Sum(summary.A = a, summary.B = b);
+                    summary.Output();
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            
+            
         }
     }
 }
